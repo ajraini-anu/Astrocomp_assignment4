fbcded5dcb2732456a76ecf0df3344c70046cb33
ajraini-anu <u8271429@anu.edu.au>
2025-10-01 19:33:23 +1000

    update dict.py

---
+++ update/dict.py
@@ -0,0 +1,8 @@
+phone_book = {"Alice": "0348938593", "Bob": "03048398", "Charlie" : "3849898594"}
+phone_book.update(Diana = 29893748374)
+
+phone_book["Bob"] = "3434354"
+del phone_book["Charlie"]
+print(phone_book)
+#print(phone_book.get("Eve"))        
+#remove the last command from the first file cause it will return None since there is no key "Eve"        
